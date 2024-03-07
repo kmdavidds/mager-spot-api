84d2d0331e23ba3e9f2e5bf36f8aa8029d791e30
kmdavidds <kmdavidds@gmail.com>
2024-03-08 05:25:45 +0700

    Update User struct fields names and tags

--- entity/user.go
+++ entity/user.go
@@ -10,12 +10,12 @@ type User struct {
 	ID               uuid.UUID `json:"id" gorm:"primary_key"`
 	Username         string    `json:"username" gorm:"not null; unique"`
 	Email            string    `json:"email" gorm:"not null; unique" validate:"email; endswith=@student.ub.ac.id;"`
-	Password         string    `json:"password" gorm:"not null"`
-	DisplayName      string    `json:"-"`
-	PhoneNumber      string    `json:"-"`
-	Address          string    `json:"-"`
-	IsSeller         bool      `json:"-"`
-	IsSubscribed     bool      `json:"-"`
-	SubscribedUntil  time.Time `json:"-"`
-	ProfilePhotoLink string    `json:"-"`
+	Password         string    `json:"-" gorm:"not null"`
+	DisplayName      string    `json:"displayName"`
+	PhoneNumber      string    `json:"phoneNumber"`
+	Address          string    `json:"Address"`
+	IsSeller         bool      `json:"isSeller"`
+	IsSubscribed     bool      `json:"isSubscribed"`
+	SubscribedUntil  time.Time `json:"subscribedUntil"`
+	ProfilePhotoLink string    `json:"profilePhotoLink"`
 }
