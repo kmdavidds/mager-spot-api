130f0e7ff9475ca13193fe82ec3c493853e38987
kmdavidds <kmdavidds@gmail.com>
2024-03-07 19:30:41 +0700

    Update PostgreSQL connection configuration

--- pkg/database/postgresql/postgresql.go
+++ pkg/database/postgresql/postgresql.go
@@ -9,8 +9,10 @@ import (
 )
 
 func ConnectDatabase() *gorm.DB {
-	dsn := os.Getenv("POSTGRESQL_DSN")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  os.Getenv("POSTGRESQL_DSN"),
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalf("Error while connecting to database: %v", err)
